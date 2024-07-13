ef4bb66e3d617d0269b130520f3d54fc2689d5cf
Evil2997 <dima.molchanow2012@gmail.com>
2024-07-13 23:27:13 +0300

    Move clayton game code with random moves

---
+++ modules/games/game_512.py
@@ -0,0 +1,40 @@
+import time
+
+import pyautogui as pg
+import random
+from modules.Timers import delay
+from modules.moves import drag_to_bottom
+from modules.screens import hunt_for_the_button_in_list
+
+
+def play_512():
+    drag_to_bottom()
+    hunt_for_the_button_in_list(Clayton_512_play)
+    delay()
+    pg.click(Clayton_512_start)
+    time_start = time.time()
+    while time.time() - time_start <= 150:
+        random_moves()
+    hunt_for_the_button_in_list(take_reward_512)
+
+
+def random_moves():
+    moves = [(940, 470), (940, 850), (700, 650), (1170, 650)]
+    start_end_pairs = [(moves[1], moves[0]), (moves[0], moves[1]), (moves[3], moves[2]), (moves[2], moves[3])]
+    start, end = random.choice(start_end_pairs)
+    move_mouse(start, end)
+
+
+def move_mouse(start, end):
+    pg.mouseUp()
+    pg.moveTo(start)
+    pg.mouseDown()
+    pg.moveTo(end)
+    pg.mouseUp()
+
+
+Clayton_512_play = ["Clayton_512_play"]
+take_reward_512 = ["take_reward_512"]
+Clayton_512_start = (940, 780)
+Clayton_512_back = (940, 880)
+
