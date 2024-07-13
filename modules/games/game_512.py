bdb70d7953bcc8dcbabb479898d48dc67d7c2da6
Evil2997 <dima.molchanow2012@gmail.com>
2024-07-13 23:32:13 +0300

    Update

--- modules/games/game_512.py
+++ modules/games/game_512.py
@@ -1,21 +1,24 @@
+import random
 import time
 
 import pyautogui as pg
-import random
+
 from modules.Timers import delay
 from modules.moves import drag_to_bottom
 from modules.screens import hunt_for_the_button_in_list
 
 
-def play_512():
+def play_512(how_much_you_want_to_play):
     drag_to_bottom()
     hunt_for_the_button_in_list(Clayton_512_play)
     delay()
-    pg.click(Clayton_512_start)
-    time_start = time.time()
-    while time.time() - time_start <= 150:
-        random_moves()
-    hunt_for_the_button_in_list(take_reward_512)
+    for game in range(how_much_you_want_to_play):
+        pg.click(Clayton_512_start)
+        time_start = time.time()
+        while time.time() - time_start <= 150:
+            random_moves()
+        hunt_for_the_button_in_list(take_reward_512)
+    pg.click(Clayton_512_back)
 
 
 def random_moves():
@@ -26,15 +29,14 @@ def random_moves():
 
 
 def move_mouse(start, end):
-    pg.mouseUp()
     pg.moveTo(start)
     pg.mouseDown()
     pg.moveTo(end)
     pg.mouseUp()
 
 
-Clayton_512_play = ["Clayton_512_play"]
-take_reward_512 = ["take_reward_512"]
-Clayton_512_start = (940, 780)
-Clayton_512_back = (940, 880)
-
+if __name__ == '__main__':
+    Clayton_512_play = ["Clayton_512_play"]
+    take_reward_512 = ["take_reward_512"]
+    Clayton_512_start = (940, 780)
+    Clayton_512_back = (940, 880)
