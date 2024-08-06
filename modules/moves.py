2385b0658637a3606a5b7656aafbc2b334ed13af
Evil2997 <dima.molchanow2012@gmail.com>
2024-08-06 20:10:44 +0300

    Update

--- modules/moves.py
+++ modules/moves.py
@@ -7,13 +7,13 @@ from modules.Timers import delay
 from modules.screens import find_it_and_click_it, find_template_on_region
 
 
-def drag_to_up(duration=0.12, cords_to_drag=cords_to_drag):
+def drag_to_up(duration=0.16, cords_to_drag=cords_to_drag):
     random_y = random.uniform(-15, 15)
     pg.moveTo(cords_to_drag[0], cords_to_drag[1])
     pg.dragTo(cords_to_drag[0], cords_to_drag[1] + random_y + 295, duration=duration)
 
 
-def drag_to_bottom(duration=0.12, cords_to_drag=cords_to_drag):
+def drag_to_bottom(duration=0.16, cords_to_drag=cords_to_drag):
     random_y = random.uniform(-15, 15)
     pg.moveTo(cords_to_drag[0], cords_to_drag[1] + random_y + 495)
     pg.dragTo(cords_to_drag[0], cords_to_drag[1], duration=duration)
