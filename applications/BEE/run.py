3373fdbacfd99e92ba6aae24f9eeddca5260f63b
Evil2997 <dima.molchanow2012@gmail.com>
2024-08-15 23:54:09 +0300

    Update

--- applications/BEE/run.py
+++ applications/BEE/run.py
@@ -5,26 +5,28 @@ from modules.screens import click_on_images
 
 
 def Run_BEE(dailik):
-    PreRun(find_BEE, chat=True, chat_type="click", chatbot_string=2)
-    # dailik
+    # PreRun(find_BEE, chat=True, chat_type="click", chatbot_string=2)
     if click_on_images(target_colors=colors_check_daily):
         pg.click(daily_BEE)
-        delay(3, 4)
+        delay()
 
     pg.click(upgrades_BEE)
-    delay(3, 4)
+    delay()
     for _ in range(3):
         drag_to_bottom(duration=0.2)
-        delay()
+        delay(0.2, 0.3)
     for _ in range(20):
-        for coordinates in upgrades_all:
-            pg.click(coordinates)
-            delay(0.02, 0.2)
-        delay()
+    #     for coordinates in upgrades_all:
+    #         pg.click(coordinates)
+    #         delay(0.02, 0.2)
+        find_it_and_click_it(bee_upgrade)
+        delay(0.25, 0.5)
     pg.click(other_menu_BEE)
     delay(3, 4)
+    pg.click(upgrades_BEE)
     for _ in range(20):
-        pg.click(upgrades_last)
-        delay()
+        # pg.click(upgrades_last)
+        find_it_and_click_it(bee_upgrade)
+        delay(0.25, 0.5)
 
     Close_AnyWay()
