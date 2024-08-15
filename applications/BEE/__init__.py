18038b83375fc3a123406a6b8cc9093ad5521539
Evil2997 <dima.molchanow2012@gmail.com>
2024-08-16 00:07:22 +0300

    Update

--- applications/BEE/__init__.py
+++ applications/BEE/__init__.py
@@ -3,8 +3,6 @@ daily_BEE = (950, 990)
 upgrades_BEE = (1150, 950)
 other_menu_BEE = (290, 950)
 
-bee_upgrade = ["bee_upgrade"]
-
 upgrades_all = [(1100, 810), (1100, 540), (1100, 270)]
 upgrades_last = (1100, 700)
 
