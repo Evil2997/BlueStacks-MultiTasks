23ff08d0105cc265fabef58303e9c5e9b3ccba80
Evil2997 <dima.molchanow2012@gmail.com>
2024-08-25 16:52:49 +0300

    Update

--- applications/Baboon/run.py
+++ applications/Baboon/run.py
@@ -4,7 +4,7 @@ from modules.moves import Close_AnyWay
 
 
 def Run_Baboon(dailik, event):
-    PreRun(find_Baboon, chat=True, chat_type="click", chatbot_string=0)
+    # PreRun(find_Baboon, chat=True, chat_type="click", chatbot_string=0)
     if dailik:
         for coordinates in Baboon_daily_reward:
             pg.click(coordinates)
@@ -14,14 +14,14 @@ def Run_Baboon(dailik, event):
             delay(0.4, 0.8)
         pg.press("Esc")
         delay()
-    for _ in range(4):
+    for _ in range(16):
         pg.press("num4")
         delay(25, 30)
         pg.press("num4")
-        delay()
+        delay(0.4, 0.6)
         for coordinates in repair_battery:
             pg.click(coordinates)
-            delay()
+            delay(0.4, 0.6)
     for coordinates in upgrade_battery:
         pg.click(coordinates)
         delay()
