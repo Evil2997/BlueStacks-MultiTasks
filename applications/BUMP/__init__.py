d852d47765a6f204dadf12889389d72f68d25e73
Evil2997 <dima.molchanow2012@gmail.com>
2024-08-31 02:53:51 +0300

    Good Update for BUMP

--- applications/BUMP/__init__.py
+++ applications/BUMP/__init__.py
@@ -3,9 +3,8 @@ find_BUMP_1 = ["BUMP"]
 find_BUMP_2 = ["BUMP_2"]
 green_X = ["green_X"]
 gray_X = ["gray_X"]
-BUMP_ready = ["BUMP_ready"]
 
 open_bust = (940, 800)
 bust_activate = [(940, 720), (940, 950)]
-
+cords_to_drag__for_BUMP = (940, 300)
 colors_daily = [(31, 125, 63)]
--- applications/BUMP/run.py
+++ applications/BUMP/run.py
@@ -32,30 +32,33 @@ def moon_bump():
 def Run_BUMP(dailik, event, win_main):
     find_BUMP = find_BUMP_2 if win_main else find_BUMP_1
 
-    PreRun(find_BUMP, chat=True, chat_type="click", chatbot_string=1)
-    dailik = True
-    for _ in range(16):
-        if dailik:
-            if click_on_images(target_colors=colors_daily):
-                dailik = False
-        delay(0.2, 0.4)
-        if dailik:
-            drag_to_bottom(duration=0.4, cords_to_drag=(940, 300))
-        else:
-            drag_to_up(duration=0.4, cords_to_drag=(940, 300))
-        delay(0.2, 0.4)
-        find_it_and_click_it(green_X)
-        find_it_and_click_it(gray_X)
-        delay(0.2, 0.4)
-        swipe_left()
-        delay(0.4, 0.6)
-        if find_it_and_click_it(BUMP_ready):
-            break
-    delay(3, 4)
+    PreRun(find_BUMP, win_main, chat=True, chat_type="click", chatbot_string=1)
+
+    claim_daily__swipe_news()
+
     if event:
         buy_bust_bump()
+
     for _ in range(16):
         pg.click(middle_screen)
         delay(0.01, 0.1)
-    # moon_bump()
+
     Close_AnyWay()
+
+
+def claim_daily__swipe_news(opposite_direction_numeric=0):
+    for i in [2, 2, 2, 1]:
+        for _ in range(i):
+            drag_to_bottom(duration=0.4, cords_to_drag=cords_to_drag__for_BUMP)
+            opposite_direction_numeric = opposite_direction_numeric + 1
+        if click_on_images(target_colors=colors_daily):
+            break
+    for _ in range(opposite_direction_numeric):
+        drag_to_up(duration=0.4, cords_to_drag=cords_to_drag__for_BUMP)
+    for _ in range(3):
+        find_it_and_click_it(green_X)
+        find_it_and_click_it(gray_X)
+        delay(0.2, 0.4)
+    for _ in range(3):
+        swipe_left(duration=0.4)
+    delay(3, 4)
