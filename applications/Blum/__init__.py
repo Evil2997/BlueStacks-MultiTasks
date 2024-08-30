b4c24d6c63aedc35fc82f149d17bdb2340288d07
Evil2997 <dima.molchanow2012@gmail.com>
2024-08-31 02:54:31 +0300

    Update

--- applications/Blum/__init__.py
+++ applications/Blum/__init__.py
@@ -1,6 +1,6 @@
 # [Blum_params]-[Start]
 find_Blum_1 = ["Blum"]
 find_Blum_2 = ["Blum_2"]
-claim_farm = (940, 840)
-claim_reward_daily = (750, 960)
+claim_farm = (730, 840)
+claim_reward_daily = (730, 960)
 # [Blum_params]-[End]
--- applications/Blum/run.py
+++ applications/Blum/run.py
@@ -6,11 +6,11 @@ from modules.moves import Close_AnyWay
 def Run_Blum(dailik, event, win_main):
     find_Blum = find_Blum_2 if win_main else find_Blum_1
 
-    PreRun(find_Blum)
+    PreRun(find_Blum, win_main)
     if dailik:
         pg.click(claim_reward_daily)
-        delay(2, 3)
+        delay(3, 4)
     for _ in range(2):
         pg.click(claim_farm)
-        delay(2, 3)
+        delay(3, 4)
     Close_AnyWay()
