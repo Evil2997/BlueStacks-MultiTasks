35a676c1562d4f5ee2a382339c0de3d6f6632386
Evil2997 <dima.molchanow2012@gmail.com>
2024-09-07 02:24:39 +0300

    Add upgrade Time Watches

--- applications/TimeFarm/__init__.py
+++ applications/TimeFarm/__init__.py
@@ -1,3 +1,7 @@
 find_TimeFarm_1 = ["TimeFarm"]
 find_TimeFarm_2 = ["TimeFarm_2"]
 FarmingTime = [(160, 950), (940, 810), (940, 810)]
+
+agree_upgrade = (240, 940)
+TimeFarm_open_upgrade = ["TimeFarm_open_upgrade"]
+TimeFarm_make_upgrade = ["TimeFarm_make_upgrade"]
--- applications/TimeFarm/run.py
+++ applications/TimeFarm/run.py
@@ -7,7 +7,20 @@ def Run_TimeFarm(dailik, event, win_main):
     find_TimeFarm = find_TimeFarm_2 if win_main else find_TimeFarm_1
 
     PreRun(find_TimeFarm, win_main, chat=True, chat_type="click", chatbot_string=1)
+
     for coordinates in FarmingTime:
         pg.click(coordinates)
         delay(6, 8)
+    hunt_for_the_button_in_list(TimeFarm_open_upgrade)
+    for _ in range(4):
+        for _ in range(2):
+            if find_it_and_click_it(TimeFarm_make_upgrade):
+                pg.click(agree_upgrade)
+                delay(0.6, 1)
+            drag_to_bottom()
+        for _ in range(2):
+            if find_it_and_click_it(TimeFarm_make_upgrade):
+                pg.click(agree_upgrade)
+                delay(0.6, 1)
+            drag_to_up()
     Close_AnyWay()
