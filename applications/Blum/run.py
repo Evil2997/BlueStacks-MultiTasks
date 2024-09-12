b4850e22b6e4375d9b36fc2bb58ab1d253701cc7
Evil2997 <dima.molchanow2012@gmail.com>
2024-09-13 02:21:40 +0300

    Update

--- applications/Blum/run.py
+++ applications/Blum/run.py
@@ -7,9 +7,11 @@ def Run_Blum(dailik, event, win_main):
     find_Blum = find_Blum_2 if win_main else find_Blum_1
 
     PreRun(find_Blum, win_main)
-    pg.click(claim_reward_daily)
-    delay(3, 4)
+
     for _ in range(2):
-        pg.click(claim_farm)
-        delay(3, 4)
-    Close_AnyWay()
+        pg.click(claim_reward_daily)
+        delay()
+        for _ in range(3):
+            pg.click(claim_farm)
+            delay()
+        Close_AnyWay()
