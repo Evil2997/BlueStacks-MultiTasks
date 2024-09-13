76a779b410fecdca64859dfd79dfecd133fdc620
Evil2997 <dima.molchanow2012@gmail.com>
2024-09-13 21:48:40 +0300

    Add auto taps

--- applications/SimpleCoin/run.py
+++ applications/SimpleCoin/run.py
@@ -5,18 +5,24 @@ from modules.moves import Close_AnyWay
 
 def Run_SimpleCoin(dailik, event, win_main):
     find_SimpleCoin = find_SimpleCoin_2 if win_main else find_SimpleCoin_1
+    num_clicker = 16 if win_main else 2
 
     PreRun(find_SimpleCoin, win_main)
+
     pg.click(news_click)
     delay()
     for _ in range(3):
         pg.click(claim_SimpleCoins)
         delay()
+
     # fortuna_ring()
-    pg.press("num4")
-    delay(16, 18)
-    pg.press("num4")
+
+    for _ in range(num_clicker):
+        auto_taps()
+        delay(0.2, 0.3)
+
     Close_AnyWay()
+
 
 def fortuna_ring():
     pg.click(fortuna_open)
@@ -26,3 +32,9 @@ def fortuna_ring():
     find_it_and_click_it(fortuna_reward)
     find_it_and_click_it(fortuna_null_reward)
     delay()
+
+
+def auto_taps():
+    pg.press("num4")
+    delay(8, 9)
+    pg.press("num4")
